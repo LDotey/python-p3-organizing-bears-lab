25a74e304a7186d270c0febbe675055d6b5124ea
Leigh Dotey <leighdotey@gmail.com>
2024-10-22 19:46:06 -0700

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 3, "F", "brown", "kind", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "grey", "bossy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, "F", "white", "shy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 3, "M", "brown", "solitary", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 5, "F", "peach", "kind", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 1, "M", "green", "grouchy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "F", "mottled", "wise", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 4, "F", "brown", "kind", 1);
+
+
+
+
+
+
